eb42afd7ad1717f78f2a63a6b5fa600259432d74
bautheac <o.bautheac@gmail.com>
2018-11-18 21:44:09 +0000

    debug

--- doc/storethat.R
+++ doc/storethat.R
@@ -1,31 +1,33 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, echo = FALSE, message = FALSE, warning = FALSE---------------
+library(storethat)
+
 knitr::opts_chunk$set(collapse = TRUE, eval = FALSE, comment = "#>")
 
 ## ----create--------------------------------------------------------------
 #  library(storethat)
 #  
-#  db_create(path = "~/data-raw/", n = 1L, verbose = FALSE)
+#  db_create(n = 1L, verbose = FALSE)
 
 ## ----store---------------------------------------------------------------
 #  library(pullit)
 #  
 #  end <- as_date("2018-10-31"); start <- end - years(2L)
-#  tickers <- c("ADM US Equity", "KHC US Equity", "XPO US Equity")
+#  tickers <- c("BP/ LN Equity", "WEIR LN Equity", "AAPL US Equity", "RNO FP Equity")
 #  equity_market <- BBG_equity_market(equity_tickers, start, end, verbose = FALSE)
 #  
 #  db_store(equity_market)
 
-## ----`check one`---------------------------------------------------------
-#  db_snapshot(instrument = "equity", book = "market", name = "ADM US Equity")
+## ----`check one`, eval = TRUE--------------------------------------------
+db_snapshot(instrument = "equity", book = "market", name = "RNO FP Equity")
 
-## ----`check all`---------------------------------------------------------
-#  db_snapshot(instrument = "equity")
+## ----`check all`, eval = TRUE--------------------------------------------
+db_snapshot(instrument = "equity", book = "market")
 
 ## ----`update one`--------------------------------------------------------
 #  storethat_update(instrument = "equity", book = "market", name = "ADM US Equity", verbose = FALSE)
 
 ## ----`update all`--------------------------------------------------------
-#  storethat_update(verbose = FALSE)
+#  storethat_update(instrument = "equity", verbose = FALSE)
 
 ## ----`delete one`--------------------------------------------------------
 #  db_delete(instrument = "equity", book = "market", name = "ADM US Equity")
